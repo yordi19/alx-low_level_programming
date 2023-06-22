a3086c7cf5c7f6be4d38393f9887c407b94a8293
yordi19 <yordileake@gmail.com>
2023-06-22 10:24:08 -0700

    0 > 972?

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,13 +16,13 @@ int largest_number(int a, int b, int c)
     {
         largest = a;
     }
-    else if (a > b && c > a)
+    else if (b > a && b > c)
     {
-        largest = c;
+        largest = b;
     }
     else
     {
-        largest = b;
+        largest = c;
     }
 
     return (largest);
